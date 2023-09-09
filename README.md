0eb55ac9cce0374197f65de4ca85fa07e6817a84
FunOrange <junarvi@yahoo.ca>
2023-09-09 17:50:50 -0400

    install rusty-tesseract, configure build tasks and launch.json for debugging

--- README.md
+++ README.md
@@ -2,4 +2,16 @@
 
 First, install prerequisites by following [this guide](https://tauri.app/v1/guides/getting-started/prerequisites).
 
-Then use `npm run tauri dev` for development, and use `npm run tauri build` to build the installer.
+### Starting the development environment
+```
+npm run tauri dev
+```
+
+### Debugging rust backend
+1. Start NextJS development server in the background by pressing Ctrl-Shift-B (this runs 'npm run dev' which is configured as the default build task)
+2. Start debugging using the 'Tauri Development Debug' configuration
+
+### Building the installer
+```
+npm run tauri build
+```
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,8 +1,47 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use std::collections::HashMap;
+
+use rusty_tesseract::{Args, Image};
+
 fn main() {
-  tauri::Builder::default()
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    // 1. Read image
+    let img = Image::from_path("img/string.png");
+
+    // 2. Set tesseract parameters
+    let default_args = Args::default();
+
+    // the default parameters are
+    /*
+    Args {
+        lang: "eng",
+        dpi: Some(150),
+        psm: Some(3),
+        oem: Some(3),
+    }
+    */
+
+    // fill your own argument struct if needed
+    // Optional arguments are ignored if set to `None`
+    let mut my_args = Args {
+        //model language (tesseract default = 'eng')
+        //available languages can be found by running 'rusty_tesseract::get_tesseract_langs()'
+        lang: "eng".to_string(),
+
+        //map of config variables
+        //this example shows a whitelist for the normal alphabet. Multiple arguments are allowed.
+        //available arguments can be found by running 'rusty_tesseract::get_tesseract_config_parameters()'
+        config_variables: HashMap::from([(
+            "tessedit_char_whitelist".into(),
+            "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".into(),
+        )]),
+        dpi: Some(150), // specify DPI for input image
+        psm: Some(6), // define page segmentation mode 6 (i.e. "Assume a single uniform block of text")
+        oem: Some(3), // define optical character recognition mode 3 (i.e. "Default, based on what is available")
+    };
+
+    tauri::Builder::default()
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
